ecc872ecb17641275e71b81e6db5b2eac788b063
sisul1204 <lizhipeng1021@163.com>
2020-07-22 21:57:00 +0800

    模板创建

--- weixin/contact/test_user.py
+++ weixin/contact/test_user.py
@@ -7,6 +7,7 @@ import json
 import logging
 import time
 
+import pystache
 import requests
 
 from weixin.contact.token import Weixin
@@ -35,6 +36,20 @@ class TestUser:
                           ).json()
         logging.debug(json.dumps(r, indent=2, ensure_ascii=False))
         assert r['errcode'] == 0
+
+    def test_create_by_template(self):
+        print(pystache.render('hello {{name}} {{#has}} world {{value}}  {{/has}}',
+                              {'name': 'hogwarts',
+                               'has': [{'value':1}, {'value':2}, {'value': 3}]
+                               }))
+
+
+    def test_list(self):
+        r = requests.get('https://qyapi.weixin.qq.com/cgi-bin/user/simplelist',
+                         params={'access_token':Weixin.get_token(),'department_id':14}
+                         ).json()
+
+        logging.debug(json.dumps(r, indent=2, ensure_ascii=False))
 
 
     
