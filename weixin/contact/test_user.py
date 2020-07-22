0ccff5400a20b5881515924ffdbc2fcec1bb33c3
lizhipeng1021 <lizhipeng1021@163.com>
2020-07-22 17:57:12 +0800

    创建成员

--- weixin/contact/test_user.py
+++ weixin/contact/test_user.py
@@ -3,10 +3,39 @@
 # Author:sisul
 # time: 2020/7/22 10:55
 # file: test_user.py
+import json
+import logging
+import time
+
+import requests
+
+from weixin.contact.token import Weixin
+
 
 class TestUser:
     depart_id = 14
 
     @classmethod
     def setup_class(cls):
-        
+        pass
+
+
+    def test_create(self):
+        uid = str(time.time())
+        data = {
+            'userid': uid,
+            'name': uid,
+            'department': [self.depart_id],
+            'email': uid + '@qq.com'
+        }
+
+        r = requests.post('https://qyapi.weixin.qq.com/cgi-bin/user/create',
+                          params={'access_token':Weixin.get_token()},
+                          json= data
+                          ).json()
+        logging.debug(json.dumps(r, indent=2, ensure_ascii=False))
+        assert r['errcode'] == 0
+
+
+    
+
