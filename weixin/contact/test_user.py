c1849ebdf97aa1ca504d91773a26a4b8137ba508
sisul1204 <lizhipeng1021@163.com>
2020-07-23 22:32:06 +0800

    使用模板pystache创建人员

--- weixin/contact/test_user.py
+++ weixin/contact/test_user.py
@@ -38,10 +38,16 @@ class TestUser:
         assert r['errcode'] == 0
 
     def test_create_by_template(self):
-        print(pystache.render('hello {{name}} {{#has}} world {{value}}  {{/has}}',
-                              {'name': 'hogwarts',
-                               'has': [{'value':1}, {'value':2}, {'value': 3}]
-                               }))
+        uid = 'nupt' + str(time.time())
+        data = self.get_user({'name':uid, 'title':'六代目火影', 'email':'111@qq.com'})
+        data = data.encode('utf-8')
+        r = requests.post('https://qyapi.weixin.qq.com/cgi-bin/user/create',
+                          params={'access_token': Weixin.get_token()},
+                          data=data,
+                          headers={'content-type':'application/json;charset=UTF-8'}
+                          ).json()
+        logging.debug(json.dumps(r, indent=2, ensure_ascii=False))
+        assert r['errcode'] == 0
 
 
     def test_list(self):
@@ -50,6 +56,16 @@ class TestUser:
                          ).json()
 
         logging.debug(json.dumps(r, indent=2, ensure_ascii=False))
+
+
+    def get_user(self,dct):
+        template = ''.join(open('user_create.json', encoding='utf-8').readlines())
+        # logging.debug(template)
+        return pystache.render(template, dct)
+
+    def test_get_user(self):
+        logging.debug(self.get_user({'name':'naruto', 'title':'火影', 'email':'111@qq.com'}))
+
 
 
     
